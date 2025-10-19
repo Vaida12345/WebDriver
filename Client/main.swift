44ba977fb58d252f07d2e297504dbc11ef8603d9
Vaida <developer@vaidaf.net>
2025-10-19 14:11:08 +1100

    ServerError now captures backtrace for debugging purposes.

--- Client/main.swift
+++ Client/main.swift
@@ -10,21 +10,25 @@ import WebDriver
 import FinderItem
 
 
-let driver = try await WebDriver.Firefox()
-    .pageLoadStrategy(.eager)
-    .profile(location: "/Users/vaida/Library/Application Support/Firefox/Profiles/2hzm7hvo.Default User")
-
-let session = try await driver.startSession()
-
-do {
-    try await session.open(url: URL(string: "https://www.google.com")!)
-    let window = try await session.window
-    let element = try await window.findElement(where: { $0.tag == "textarea1234567" && $0.title == "Search" })
-    try await element.write("Swift")
+func main() async throws {
+    let driver = try await WebDriver.Firefox()
+        .pageLoadStrategy(.eager)
+        .profile(location: "/Users/vaida/Library/Application Support/Firefox/Profiles/2hzm7hvo.Default User")
     
-//    try await Task.sleep(for: .seconds(10))
-} catch {
-    print(error)
+    let session = try await driver.startSession()
+    
+    do {
+        try await session.open(url: URL(string: "https://www.google.com")!)
+        let window = try await session.window
+        let element = try await window.findElement(where: { $0.tag == "textarea1234567" && $0.title == "Search" })
+        try await element.write("Swift")
+        
+        //    try await Task.sleep(for: .seconds(10))
+    } catch {
+        print(error)
+    }
+    
+    try await session.close()
 }
 
-try await session.close()
+try await main()
